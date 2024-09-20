f88b52e57acf04b902a720de3a8c674baf15994c
William Sutcliffe <william.sutcliffe@cern.ch>
2024-09-20 14:12:13 +0200

    - retrained neural network now SBT x, y, z removed - new weights added SBTveto_vacuum_multiclass_NN_SBT_E_signal_xyz.pth - modified nn_output in sbtveto.util.inference - modified apply_nn

--- apply_nn.py
+++ apply_nn.py
@@ -6,33 +6,43 @@ import joblib
 import glob
 import uproot
 
-
-XYZ =np.load("../SBT_XYZ.npy")
+# open some example neuDIS data
 neu_files = glob.glob("preprocess/*neuDIS*.root")
+#neu_files = glob.glob("../SBT/ml_dataset/*neuDIS*.root")
+
 file = uproot.open(neu_files[0])
 #inputmatrix = np.array(file['tree;1']['inputmatrix'].array())[:,:-1]
 data = file['tree;1']['inputmatrix'].array(library='np')
-inputmatrix = data[:, :-1]
+
+# for input matrix we remove last two columns no. ubt cells and spill weight
+inputmatrix = data[:, :-2]
 event_weights = data[:, -1]
+
 print(f"Input is {len(inputmatrix)} events of nuDIS")
 print("Input shape ", inputmatrix.shape)
 print("Features are E for 2000 SBT cells, signal vertex, x, y, z and UBT hits.")
 print("The model will also use SBT x, y, z of cells")
+
+# load scalar this could also be done with a json
 scaler_loaded = joblib.load('data/robust_scaler.pkl')
-model = NN(8004,3,[32,32,32,16,8], dropout=0)
+
+# define model
+model = NN(2003,3,[32,32,32,16,8], dropout=0)
 
 # Load the state dictionary into the model
-model.load_model('data/SBTveto_vacuum_multiclass_NN.pth')
+model.load_model('data/SBTveto_vacuum_multiclass_NN_SBT_E_signal_xyz.pth')
 
 # Now the model is ready to use
 print(model)
 model.eval()
 
-outputs, decisions,classification = nn_output(model, inputmatrix, XYZ, scaler_loaded)#returns True if to be removed
+# Apply model
+outputs, decisions,classification = nn_output(model, inputmatrix, scaler_loaded)#returns True if to be removed
 
 print("The SBT decisions are ", decisions)
 
 # Calculate sum of event weights where decision is False
 #print(event_weights)
+
 sum_weights_false = np.sum(event_weights[~decisions])
 print("Number of signal events weighted over 5 years: ", sum_weights_false)
--- sbtveto/util/inference.py
+++ sbtveto/util/inference.py
@@ -3,16 +3,13 @@ import torch
 from torch_geometric.nn import knn
 from torch_geometric.data import Data
 
-def nn_output(model, data, sbt_xyz, scalar):
+def nn_output(model, data, scalar):
     
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model.to(device)
-    X=np.hstack([ data  , np.repeat(sbt_xyz[:1,:],data.shape[0],0),
-              np.repeat(sbt_xyz[1:2,:],data.shape[0],0),
-              np.repeat(sbt_xyz[2:,:],data.shape[0],0)])
-    X = scalar.transform(X)
-    X = torch.tensor(X, dtype =torch.float32 ).to(device)
-    output = model(X)
+    x = scalar.transform(data)
+    x = torch.tensor(x, dtype =torch.float32 ).to(device)
+    output = model(x)
     sbt_decision = (torch.max(output, dim = 1).indices != 0)#veto returns True if not signal(0)
     classification=torch.max(output, dim = 1).indices
     return output, sbt_decision,classification
