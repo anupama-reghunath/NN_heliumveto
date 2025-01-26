5b12e24d13bc56735a5b2c00755b456d4926f7c9
anupama-reghunath <anupamareghu@gmail.com>
2025-01-26 13:28:58 +0100

    Add condor scripts for preprocessing

---
+++ data_preprocessing/condor_scripts/signal_condor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#######################################################################################
+
+#ClusterId	=	$1
+#ProcId		=	$2
+#Runnumber	=	$3
+
+#######################################################################################
+source /cvmfs/ship.cern.ch/24.10/setUp.sh 
+#alienv load FairShip/latest-master-release > config_<version>.sh
+source /afs/cern.ch/user/a/areghuna/config_ECN3_2024.sh
+echo 'config sourced'
+#######################################################################################
+python /afs/cern.ch/user/a/areghuna/William/sbtveto/data_preprocessing/signal_MuBack_dataformatter.py --jobDir "$3"
+#should always have the whole path for condor to work
+
+xrdcp datafile_signalEMBG_*.h5 /eos/experiment/ship/user/anupamar/NN_data/h5_files/
+xrdcp datafile_signalEMBG_*.root /eos/experiment/ship/user/anupamar/NN_data/root_files/
+
